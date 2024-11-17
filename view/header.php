f5921b780f0fae9293f2f190e2bda9a558e428c2
Huynh Ngoc Phuc <162518952+phuchuynh99@users.noreply.github.com>
2024-11-17 18:55:59 +0700

    up

--- view/header.php
+++ view/header.php
@@ -18,6 +18,9 @@
     <link rel="stylesheet" href="public/css/products.css">
     <link rel="stylesheet" href="public/css/product-detail.css">
     <link rel="stylesheet" href="public/css/font.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
 </head>
 
