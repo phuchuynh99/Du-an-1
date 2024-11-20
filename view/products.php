26ed22854d0e2438b9131fd4cc2a70d59f663af2
baopibo123 <baonguyenpk117@gmail.com>
2024-11-20 21:15:55 +0700

    up product

--- view/products.php
+++ view/products.php
@@ -4,7 +4,7 @@ foreach ($catalog_list as $category) {
     extract($category);
     $cataloglist .= '<li>
                         <label class="d-flex align-items-center">
-                            <input type="checkbox" class="me-2">
+                           
                             <a href="#" class="text-decoration-none text-dark">'.$name.'</a>
                         </label>
                      </li>';
@@ -14,7 +14,7 @@ foreach ($catalog_list as $category) {
 <section class="products" id="products">
     <div class="row">
         <!-- Sidebar -->
-        <div class="product-col4 col-md-3 mb-4">
+        <div class="product-col4">
             <aside class="product-sidebar">
                 <div class="product-category bg-white p-3 rounded shadow-sm">
                     <h4 class="product-category-title fw-bold">DANH MỤC</h4>
@@ -26,7 +26,7 @@ foreach ($catalog_list as $category) {
         </div>
 
         <!-- Product -->
-        <div class="product-col6 col-md-9">
+        <div class="product-col6">
             <div class="row g-4">
                 <?php 
                     foreach ($productlist as $product) {
@@ -34,7 +34,7 @@ foreach ($catalog_list as $category) {
                         if ($img != "") {
                             $img = './' . PATH_IMG . './' . $img;
                         }
-                        echo '<div class="col-md-3 col-sm-6">
+                        echo '
                                 <div class="product-card text-center shadow-sm p-3">
                                     <a href="product-detail" class="text-decoration-none text-dark">
                                         <img src="' . $img . '" class="product-img img-fluid" alt="' . $name . '">
@@ -44,16 +44,16 @@ foreach ($catalog_list as $category) {
                                     <p class="product-discount-price text-danger fw-bold">' . $price . '</p>
                                     <div class="product-actions d-flex justify-content-center align-items-center gap-2 mt-3">
                                         <button class="product-btn-cart d-flex align-items-center">
-                                            <i class="bi bi-cart"></i>
+                                            <i class="fa-regular fa-cart-plus"></i>
                                         </button>
                                         <button class="product-btn-buy">Mua ngay</button>
                                     </div>
-                                </div>
-                            </div>';
+                                </div>';
                     }
                 ?>
             </div>
         </div>
     </div>
 </section>
+
 
