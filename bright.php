db52729d126c55dd6dd18380ebc8a669d7ec5bad
tantruc69 <tantrucfpt03@gmail.com>
2024-11-21 23:48:15 +0700

    retore

--- bright.php
+++ bright.php
@@ -6,19 +6,24 @@ require_once('model/product.php');
 require_once('model/coupon.php');
 require_once('model/user.php');
 require_once('model/bill.php');
-require_once('model/jwt.php');
-require_once('model/mailer_helper.php');
+// require_once('model/jwt.php');
+// require_once('model/mailer_helper.php');
 
 
 //mailer
-require_once('jwt/Key.php');
-require_once('jwt/JWTExceptionWithPayloadInterface.php');
-require_once('jwt/BeforeValidException.php');
-require_once('jwt/ExpiredException.php');
-require_once('jwt/SignatureInvalidException.php');
-require_once('jwt/JWT.php');
-require_once('model/jwt.php');
+// require_once('jwt/Key.php');
+// require_once('jwt/JWTExceptionWithPayloadInterface.php');
+// require_once('jwt/BeforeValidException.php');
+// require_once('jwt/ExpiredException.php');
+// require_once('jwt/SignatureInvalidException.php');
+// require_once('jwt/JWT.php');
+// require_once('model/jwt.php');
 
 //view
 require_once('view/global.php');
+<<<<<<< Updated upstream
 require_once './vendor/autoload.php';
+=======
+// require_once './vendor/autoload.php';
+?>
+>>>>>>> Stashed changes
